428176d3dd39e4eb6c79f0ae4614435d5dcde2a8
Mario Cortes <mcortes.1113@gmail.com>
2020-01-30 21:39:39 -0600

    Updates

---
+++ precache-manifest.e334d3e2d35355d26d39946ade512dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddfd4ffa0bef8802d124069eead96e4",
+    "url": "/userDirectoryV2/index.html"
+  },
+  {
+    "revision": "6c94a9b31f5e9ebfb8d7",
+    "url": "/userDirectoryV2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "73a5a81b94e4e33c5017",
+    "url": "/userDirectoryV2/static/js/2.76dc14d4.chunk.js"
+  },
+  {
+    "revision": "6c94a9b31f5e9ebfb8d7",
+    "url": "/userDirectoryV2/static/js/main.a4bd07da.chunk.js"
+  },
+  {
+    "revision": "ffac089b982387081178",
+    "url": "/userDirectoryV2/static/js/runtime-main.cc8034a9.js"
+  }
+]);
